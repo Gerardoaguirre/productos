c34cae7dc6654dcc3f055eb18696f191a80302cd
Gerardoaguirre <gecoaguirre@hotmail.com>
2023-08-27 21:28:30 -0600

    se agregan condificones para realizar errores y usar circuitbreaker en items (id-2)

--- src/main/java/com/informacionbdi/springboot/app/productos/controllers/ProductoController.java
+++ src/main/java/com/informacionbdi/springboot/app/productos/controllers/ProductoController.java
@@ -1,11 +1,12 @@
 package com.informacionbdi.springboot.app.productos.controllers;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-//import org.springframework.core.env.Environment;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,8 +17,8 @@ import com.informacionbdi.springboot.app.productos.models.service.IProductoServi
 @RestController
 public class ProductoController {
 	
-	//@Autowired
-	//private Environment env;
+	@Autowired
+	private Environment env;
 	
 	@Value("${server.port}")
 	private Integer port;
@@ -28,23 +29,23 @@ public class ProductoController {
 	@GetMapping("/listar")
 	public List<Producto> listar(){
 		return productoService.findAll().stream().map(producto ->{
-			//producto.setPort(Integer.parseInt(env.getProperty("local.server.port")));
-			producto.setPort(port);
+			producto.setPort(Integer.parseInt(env.getProperty("local.server.port")));
+			//producto.setPort(port);
 			return producto;
 		}).collect(Collectors.toList());
 	}
 	
 	@GetMapping("/ver/{id}")
-	public Producto detalle(@PathVariable Long id) {
-		Producto producto = productoService.findById(id);
-		//producto.setPort(Integer.parseInt(env.getProperty("local.server.port")));
-		producto.setPort(port);
-	
-		boolean ok=false; //esto para simular un erro u poner en practica histryx implementado en servicio item que es quien consume este micro
-		if(!ok) {
-			throw new RuntimeException("No se pudo cargar el producto");
+	public Producto detalle(@PathVariable Long id) throws InterruptedException {
+		if(id.equals(10L)) {
+			throw new IllegalStateException("producto no encontrado"); //para simular otro error y probar circuit breaker implementado en servicio item que es quien consume este micro
 		}
-		
+		if(id.equals(7L)) {
+			TimeUnit.SECONDS.sleep(5L);
+		}
+		Producto producto = productoService.findById(id);
+		producto.setPort(Integer.parseInt(env.getProperty("local.server.port")));
+		//producto.setPort(port);
 
 		return producto;
 	}
